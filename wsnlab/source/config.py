641b9ffd5a8d0355a88a05d7e76ba06c41e72a99
mocalab <yozturk@mocalab.com>
2021-11-22 11:42:42 -0800

    updated

--- wsnlab/source/config.py
+++ wsnlab/source/config.py
@@ -6,18 +6,16 @@ BROADCAST_NODE_ADDR = 255
 
 
 ## node properties
-NODE_TX_RANGE = 150  # transmission range of nodes
+NODE_TX_RANGE = 100  # transmission range of nodes
 NODE_ARRIVAL_MAX = 200  # max time to wake up
-
-
 
 
 ## simulation properties
 SIM_NODE_COUNT = 100  # noce count in simulation
-SIM_NODE_PLACING_CELL_SIZE = 60  # cell size to place one node
+SIM_NODE_PLACING_CELL_SIZE = 40  # cell size to place one node
 SIM_DURATION = 500  # simulation Duration in seconds
 SIM_TIME_SCALE = 0.00001  #  The real time dureation of 1 second simualtion time
-SIM_TERRAIN_SIZE = (700, 700)  #terrain size
+SIM_TERRAIN_SIZE = (1400, 600)  #terrain size
 SIM_TITLE = 'Data Collection Tree'  # title of visualization window
 SIM_VISUALIZATION = True  # visualization active
 
@@ -25,4 +23,4 @@ SIM_VISUALIZATION = True  # visualization active
 
 ## application properties
 HEARTH_BEAT_TIME_INTERVAL = 15
-REPAIRING_METHOD = 'ALL_ORPHAN' # 'ALL_ORPHAN', 'FIND_ANOTHER_PARENT'
+REPAIRING_METHOD = 'FIND_ANOTHER_PARENT' # 'ALL_ORPHAN', 'FIND_ANOTHER_PARENT'
